74b0c3289257a6fe4c4fc958a8d22c2e18142602
lcs <lcs.shun@foxmail.com>
2022-01-15 06:02:10 -0800

    learn

--- tool_packs/gorm/model/model.go
+++ tool_packs/gorm/model/model.go
@@ -3,5 +3,5 @@ package model
 import "gorm.io/gorm"
 
 func AutoMigrateModels(db *gorm.DB) error {
-	return db.AutoMigrate(&User{}, &Result{})
+	return db.AutoMigrate(&User{}, &Result{}, &Member{}, &Company{})
 }
