a38af2f3ad21afe6ee42377041c11700d3975458
lcs <lcs.shun@foxmail.com>
2022-02-27 05:53:24 -0800

    learn

---
+++ go_packs/os/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	hostName, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("hostName:", hostName)
+
+	// environ := os.Environ()
+	// fmt.Println("environ: \n", environ)
+
+	goRoot := os.Getenv("GOROOT")
+	fmt.Println("the go root path:", goRoot)
+
+	goPath := os.Getenv("GOPATH")
+	fmt.Println("the go path:", goPath)
+
+	path := os.Getenv("PATH")
+	fmt.Println("path:", path)
+
+	pid := os.Getpid()
+	fmt.Println("process id:", pid)
+
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("worksplce dirtory: ", wd)
+
+	err = os.Mkdir("./hello", os.ModeDir)
+	if err != nil {
+		panic(err)
+	}
+
+	err = os.MkdirAll("./hello2/heihei", os.ModeDir)
+	if err != nil {
+		panic(err)
+	}
+}
